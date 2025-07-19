5a4ad5fec56b5d811513f94466ea0d3eb0a1c5cd
Huzaifah Tahir <f2024266232@umt.edu.pk>
2025-07-19 08:51:37 +0500

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -1,7 +1,7 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
 import { storage } from "./storage.js";
-import { insertTeamSchema, insertChallengeSchema, insertSubmissionSchema, loginSchema } from "@shared/schema.js";
+import { insertTeamSchema, insertChallengeSchema, insertSubmissionSchema, loginSchema } from "../shared/schema.js";
 import bcrypt from "bcryptjs";
 import session from "express-session";
 import MemoryStore from "memorystore";
